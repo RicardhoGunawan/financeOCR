a4d3ea0e92cb85a403e375d1eb5412fa57982b01
Ricardho gunawan <ricardho.gunawan@gmail.com>
2025-10-13 17:44:57 +0700

    fix handledelete in badget

--- app/dashboard/budgets/page.tsx
+++ app/dashboard/budgets/page.tsx
@@ -506,7 +506,7 @@ export default function BudgetsPage() {
                     <ConfirmDialog
                       title="Are you sure?"
                       description="This action will permanently remove the selected data from the system. Please confirm to continue."
-                      onConfirm={() => handleDelete(transaction)}
+                      onConfirm={() => handleDelete(budget.id)}
                       confirmText="Delete"
                       isDestructive={true} // This will apply the red style to the delete button
                     >
