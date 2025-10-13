6a35fb7d41792a8f8488e49e3c8c5c957fbaef28
Ricardho gunawan <ricardho.gunawan@gmail.com>
2025-10-13 17:49:28 +0700

    fix: category.id

--- app/dashboard/categories/page.tsx
+++ app/dashboard/categories/page.tsx
@@ -311,7 +311,7 @@ export default function CategoriesPage() {
                       <ConfirmDialog
                         title="Are you sure?"
                         description="This action will permanently remove the selected data from the system. Please confirm to continue."
-                        onConfirm={() => handleDelete(transaction)}
+                        onConfirm={() => handleDelete(category.id)}
                         confirmText="Delete"
                         isDestructive={true} // This will apply the red style to the delete button
                       >
