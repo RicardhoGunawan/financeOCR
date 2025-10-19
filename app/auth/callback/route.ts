49e03ab71c51b6b87873c1e707ebd07b8b0a1584
RicardhoGunawan <ricardho.gunawann@gmail.com>
2025-10-19 16:15:30 +0700

    fix2 in auth

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -15,15 +15,15 @@ export async function GET(request: Request) {
 
     // Pastikan 'code' ada di URL
     if (code) {
-        const cookieStore = cookies(); // Dapatkan cookie store
-
+        const cookieStore = await cookies();
         // 2. Buat Supabase client khusus untuk Route Handler
         const supabase = createServerClient(
             process.env.NEXT_PUBLIC_SUPABASE_URL!,
             process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
             {
                 cookies: {
-                    // Teruskan fungsi getter dan setter untuk cookie
+                    // Sekarang cookieStore adalah objek, 
+                    // sehingga .get(), .set(), dan .delete() akan berfungsi
                     get(name: string) {
                         return cookieStore.get(name)?.value;
                     },
